b63757e85383cc2dbb39fd882b3ce65f059680a7
TerasXgod <zzoo113258@gmail.com>
2026-04-23 22:33:50 +0300

    feat: adding event-contract

---
+++ common/src/main/kotlin/com/terasxgod/common/kafka/KafkaTopics.kt
@@ -0,0 +1,8 @@
+package com.terasxgod.common.kafka
+
+object KafkaTopics {
+	const val EMAIL_NOTIFICATIONS_V1 = "notifications.email.v1"
+	const val EMAIL_NOTIFICATIONS_DLT_V1 = "notifications.email.v1.dlt"
+
+	const val NOTIFICATION_SERVICE_GROUP = "notification-service"
+}
