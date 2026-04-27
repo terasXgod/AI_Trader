0f2edd1dd2cf5084527a0bfa0676136c4440091c
TerasXgod <zzoo113258@gmail.com>
2026-04-27 20:52:19 +0300

    feat: adding smtp sender

--- notification-service/src/main/kotlin/com/terasxgod/notification_service/service/LoggingEmailSenderService.kt
+++ notification-service/src/main/kotlin/com/terasxgod/notification_service/service/LoggingEmailSenderService.kt
@@ -3,7 +3,6 @@ package com.terasxgod.notification_service.service
 import com.terasxgod.common.events.EmailNotificationEvent
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Service
 
 @Service
